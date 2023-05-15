8ac4895b0b86bec8601c654a5329a5a838ce5dca
dejourford <dejourford@my.unt.edu>
2023-05-15 15:48:05 -0500

    create use state function for array

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import "./app.css"
+import React from "react"
+import { useState } from "react"
 
 
-export default function App() {
+export default function App () {
+  const [thingsArray, setThingsArray] = React.useState(['Thing 1', 'Thing 2']) 
+  const thingsElements = thingsArray.map(function(item) {
+    return <p key={item}>{item}</p>
+  })
+  
+  const addNewItem = () => {
+    setThingsArray(prevThingsArray => [...prevThingsArray, `Thing ${prevThingsArray.length + 1}`])
+  }
+
   return (
     <div className="container">
-        <button>Add Item</button>
+      <button onClick={addNewItem}>Add Item</button>
+      <div className="itemList">
+        {thingsElements}
+      </div>
     </div>
   )
 }
